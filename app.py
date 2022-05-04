341dd18268d4cfb48320e69514a1aa17ee8cf67c
Paolo Morey <valentinquez64@gmail.com>
2022-05-03 19:45:25 -0500

    Commit 1.3. - Login Manager

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sys
 from flask import Flask, redirect, request, render_template, jsonify, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from models import *
 
 # Config
 app = Flask(__name__)
@@ -12,6 +14,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jjoc:1234@localhost:5432/m
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "/login"
 
 
 # Models
@@ -19,9 +24,36 @@ migrate = Migrate(app, db)
 
 
 # Controllers
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(user_id)
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        try:
+            data = request.json
+            username = data["username"]
+            password = data["password"]
+            user = Usuario(usuario=username)
+            print(user)
+            user.set_password(password)
+            db.session.add(user)
+            db.session.commit()
+            load_user(user.usuario)
+            return redirect(url_for("index"))
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+            abort(500)
+        finally:
+            db.session.close()
+
+    return render_template("register.html")
 
 
 # Run
