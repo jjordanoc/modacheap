21b469a893d24deba1b0473c133ef83ae9a3533b
EnzoCam <enzo.camizan@utec.edu.pe>
2022-05-03 19:40:32 -0500

    Agregando la ruta Producto

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 # Imports
+from crypt import methods
+from  models import Producto
 import json
 import sys
+from urllib import response
 from flask import Flask, redirect, request, render_template, jsonify, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
 # Config
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jjoc:1234@localhost:5432/modacheap"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123@localhost:5432/modacheap"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-# Models
+#migrate = Migrate(app, db)
 
 
 
@@ -22,6 +22,58 @@ migrate = Migrate(app, db)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+#Cambiar route
+@app.route("/producto/create" , methods=['POST'])
+#Cambiar def
+def create_ropa():
+    error = False
+    response = {}
+    try:
+        id = request.get_json()['id']
+        usuario = request.get_json()['usuario']
+        precio = request.get_json()['precio']
+        descripcion = request.get_json()['descripcion']
+        talla = request.get_json()['talla']
+        sexo = request.get_json()['sexo']
+        categoria = request.get_json()['categoria']
+        distrito = request.get_json()['distrito']
+        productos = Producto (
+            id = id,
+            usuario = usuario,
+            precio = precio,
+            descripcion = descripcion,
+            talla = talla,
+            sexo = sexo,
+            categoria = categoria,
+            distrito = distrito
+        )
+        db.session.add(productos)
+        db.session.commit()
+        response = {
+            'id' : id,
+            'usuario' : usuario,
+            'precio' : precio,
+            'descripcion' : descripcion,
+            'talla' : talla,
+            'sexo' : sexo,
+            'categoria' : categoria,
+            'distrito' : distrito       
+        }
+    except Exception as e :
+        error=True
+        print(e)
+        print(sys.exc_info())
+        db.session.rollback()
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else: 
+        return jsonify(response)
+
+
 
 
 # Run
