28d7d019b4c79b37a5cf919debc5c3828ba0bc00
Paolo Morey <valentinquez64@gmail.com>
2022-05-03 19:14:07 -0500

    First Commit - Modelo Usuario

---
+++ models.py
@@ -0,0 +1,6 @@
+class Usuario(db.Model):
+    __tablename__ = 'users'
+    _usuario_ = db.Column(db.String(), primary_key=True, nullable=False)
+    clave = db.Column(db.String(), primary_key=False, nullable=False)
+    nombre = db.Column(db.String(), primary_key=False, nullable=False)
+    celular = db.Column(db.Integer, primary_key=False, nullable=False)
