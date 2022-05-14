e982f43a9ddd163930eda63869776ddbab68d5e7
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-05-14 12:31:24 -0500

    Se incorporo archivo scripts a parte

---
+++ static/scripts/main.js
@@ -0,0 +1,37 @@
+const usuarioLogin = function(e) {
+    e.preventDefault();
+    const correo = document.getElementById("correo");
+    const clave = document.getElementById("clave");
+    fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({
+            correo: correo.value,
+            clave: clave.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        redirect: "follow"
+    }).then(function(res) {
+        
+    });
+}
+
+const usuarioRegistrar = function(e) {
+    e.preventDefault();
+    const correo = document.getElementById("correo");
+    const clave = document.getElementById("clave");
+    fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({
+            correo: correo.value,
+            clave: clave.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        redirect: "follow"
+    }).then(function(res) {
+        
+    });
+}
