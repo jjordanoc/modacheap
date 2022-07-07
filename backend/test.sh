04e0bc1e6da9e699accbbe260cab188f63d899d0
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-07-07 07:11:54 -0500

    Run y test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -1,7 +1,7 @@
 source venv/bin/activate
 export SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
 export UPLOAD_FOLDER=static/uploaded/
-export DATABASE_URI=postgresql://postgres:0788@localhost:5432/modacheap_v2_test
+export DATABASE_URI=postgresql://jjoc:1234@localhost:5432/modacheap_v2_test
 export FLASK_APP=server
 export FLASK_ENV=development
 python3 -m unittest "tests/test_app.py"
